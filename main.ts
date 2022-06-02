6344c7d476311a510e5f0c112594dc274ffbc50d
1711071 <104405290+1711071@users.noreply.github.com>
2022-06-02 10:10:31 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
+music.setVolume(255)
 basic.forever(function () {
-	
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+})
+basic.forever(function () {
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 5) {
+    	
+    }
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(247, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(175, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(175, music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(247, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    basic.pause(200)
 })
